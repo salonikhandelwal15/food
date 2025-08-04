6daead3389afd3bde6914ffa2f329c9cdca29583
Saloni <salonikhandelwal22553@gmail.com>
2025-08-04 19:14:06 +0530

    modified the footer section

--- food-app/src/components/Layouts/Footer.js
+++ food-app/src/components/Layouts/Footer.js
@@ -95,7 +95,7 @@ function Footer(){
                         <ul className='listunstyled text-center mb-0'>
                             <li>
                                 <Link to="/">
-                                @ 2025 <span> SmashLab Burgers</span>. All Rights Reserved
+                                © 2025 <span> SmashLab Burgers</span>. All Rights Reserved
                                 </Link>
                             </li>
                             <li>
